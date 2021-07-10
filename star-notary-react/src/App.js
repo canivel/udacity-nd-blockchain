0b25e91973bbef364fe6b96cb63a94112714b06f
Danilo Canivel <d.canivel@gmail.com>
2021-07-10 19:23:16 -0400

    truffle test

--- star-notary-react/src/App.js
+++ star-notary-react/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import starNotaryArtifact from "./abis/StarNotary.json"; // Importing the JSON representation of the Smart Contract
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [meta, setMeta] = useState("");
+  const [status, setStatus] = useState("");
+  const [starOwner, setStarOwner] = useState("");
+  const [starName, setStarName] = useState("");
+
+  useEffect(async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+
+    // get contract instance
+    const networkId = await web3.eth.net.getId(); //This method find the network id to retrieve the configuration from truffle-config.js file
+    const deployedNetwork = starNotaryArtifact.networks[networkId]; // Retrieve the Network configuration from truffle-config.js file
+    setMeta(
+      new web3.eth.Contract(starNotaryArtifact.abi, deployedNetwork.address) // Initializing the contract
+    );
+
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  });
+
+  // function called to show the starName
+  async function starNameFunc() {
+    const { starName } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    const response = await starName().call(); // calling the starName property from your Smart Contract.
+    setStarName(response);
+    return starName;
+  }
+
+  // function called to show the starOwner
+  async function starOwnerFunc() {
+    const { starOwner } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    const response = await starOwner().call(); // calling the starOwner property from your Smart Contract.
+    setStarOwner(response);
+    return starOwner;
+  }
+
+  // function called to claim a Star
+  async function claimStarFunc() {
+    const { claimStar, starOwner } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    await claimStar().send({ from: account }); // Use `send` instead of `call` when you called a function in your Smart Contract
+    const response = await starOwner().call();
+    setStatus("New Star Owner is " + response + ".");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>StarNotary DAPP</h1>
+      <br />
+      <label for="name">Star Name:</label>
+      <h3 id="name">Star Name: {starName}</h3>
+      <br />
+      <br />
+      <button onclick="starNameFunc">Get Star Name</button>
+      <hr />
+      <br />
+      <label for="owner">Star Owner:</label>
+      <h3 id="owner">Star Owner: {() => starOwnerFunc}</h3>
+      <br />
+      <br />
+      <button onclick="starOwnerFunc">Get Star Owner</button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h1>Claim Star</h1>
+      <br />
+      <br />
+      <button id="claimStar" onclick="claimStarFunc">
+        Claim a Star
+      </button>
+      <br />
+      <br />
+      <br />
+      <hr />
+      <span id="status">{status}</span>
+      <br />
     </div>
   );
 }
-
-export default App;
