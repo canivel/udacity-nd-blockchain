f540cad2415ed69f558c2e8aee34fe991470e921
Danilo Canivel <d.canivel@gmail.com>
2021-07-10 20:52:20 -0400

    star notary

--- star-notary-react/src/App.js
+++ star-notary-react/src/App.js
@@ -2,30 +2,34 @@ import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import starNotaryArtifact from "./abis/StarNotary.json"; // Importing the JSON representation of the Smart Contract
 
-function App() {
-  const [account, setAccount] = useState("");
-  const [meta, setMeta] = useState("");
+const App = () => {
+  const [state, setState] = useState({
+    web3: null, accounts: null, contract: null
+  });
   const [status, setStatus] = useState("");
   const [starOwner, setStarOwner] = useState("");
   const [starName, setStarName] = useState("");
 
   useEffect(async () => {
-    const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
-
-    // get contract instance
-    const networkId = await web3.eth.net.getId(); //This method find the network id to retrieve the configuration from truffle-config.js file
-    const deployedNetwork = starNotaryArtifact.networks[networkId]; // Retrieve the Network configuration from truffle-config.js file
-    setMeta(
-      new web3.eth.Contract(starNotaryArtifact.abi, deployedNetwork.address) // Initializing the contract
-    );
-
-    const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-  });
+    const init = async () => {
+      try {
+        const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+        const accounts = await web3.eth.getAccounts();
+        const networkId = 5777 //await web3.eth.net.getId(); //This method find the network id to retrieve the configuration from truffle-config.js file
+        const deployedNetwork = starNotaryArtifact.networks[networkId]; // Retrieve the Network configuration from truffle-config.js file
+        const contract = new web3.eth.Contract(starNotaryArtifact.abi, deployedNetwork.address) // Initializing the contract
+        setState({web3, accounts, contract});
+      } catch(error) {
+        alert('Failed to load web3, accounts or contract')
+        console.error(error)
+      }
+    }
+    init();
+  }, []);
 
   // function called to show the starName
   async function starNameFunc() {
-    const { starName } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    const { starName } = state.contract.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
     const response = await starName().call(); // calling the starName property from your Smart Contract.
     setStarName(response);
     return starName;
@@ -33,7 +37,7 @@ function App() {
 
   // function called to show the starOwner
   async function starOwnerFunc() {
-    const { starOwner } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    const { starOwner } = state.contract.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
     const response = await starOwner().call(); // calling the starOwner property from your Smart Contract.
     setStarOwner(response);
     return starOwner;
@@ -41,8 +45,8 @@ function App() {
 
   // function called to claim a Star
   async function claimStarFunc() {
-    const { claimStar, starOwner } = meta.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
-    await claimStar().send({ from: account }); // Use `send` instead of `call` when you called a function in your Smart Contract
+    const { claimStar, starOwner } = state.contract.methods; // to be able to use the functions in your Smart Contract use destructuring to get the function to be call
+    await claimStar().send({ from: state.accounts[0] }); // Use `send` instead of `call` when you called a function in your Smart Contract
     const response = await starOwner().call();
     setStatus("New Star Owner is " + response + ".");
   }
@@ -82,3 +86,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
